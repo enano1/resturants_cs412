c445f16b89e6553fa9287772381ae1bc5e81bf7c
Paul Martin Enano <paul.m.enano@gmail.com>
2024-09-28 10:47:43 -0400

    test

--- cs412/settings.py
+++ cs412/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "resturant", #new app
+    "restaurant", #new app
 ]
 
 MIDDLEWARE = [
